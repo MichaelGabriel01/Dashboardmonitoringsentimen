0ceb8eca4ec5d4068a4cc601f8c5626bbbea2cb3
MichaelGabriel01 <92856821+MichaelGabriel01@users.noreply.github.com>
2026-09-18 15:20:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def auto_update(limit_latest=300):
 def load_teacher_model():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     tokenizer = AutoTokenizer.from_pretrained("indolem/indobertweet-base-uncased")
-    model = AutoModelForSequenceClassification.from_pretrained(MODEL_DIR)
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_DIR, force_download=True)
     model.to(device)
     model.eval()
     return tokenizer, model, device
